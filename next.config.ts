162cf3fc0958d9a251fab020788cfd82088771c4
yassann20 <kaiseif4e@gmail.com>
2025-07-03 19:06:43 +0900

    fix:記事サムネイル主直処理修正

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "webyayasu.sakura.ne.jp",
+        pathname: "/uthuyomizyuku/wp-content/uploads/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
